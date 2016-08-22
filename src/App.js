16de93eb6dfef997f2d287bc6e4558535e9c944a
ramonavic <ramon.hofmeister@gmail.com>
2016-08-22 15:11:17 +0200

    clean up whitespace

--- src/App.js
+++ src/App.js
@@ -9,21 +9,19 @@ import TutorialForm from './components/tutorialForm'
 class App extends React.Component {
 
 
-    render() {
-
-
-        return (
-          <div>
-            <Router history={hashHistory}>
-              <Route path ='/' component={Login} />
-              <Route path = '/home' component={Home} />
-              <Route path ='/login' component={Login} />
-              <Route path ='/add' component={TutorialForm} />
-              <Route path='*' component={Home} />
-            </Router>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Router history={hashHistory}>
+          <Route path ='/' component={Login} />
+          <Route path = '/home' component={Home} />
+          <Route path ='/login' component={Login} />
+          <Route path ='/add' component={TutorialForm} />
+          <Route path='*' component={Home} />
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
