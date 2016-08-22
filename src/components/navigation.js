f90f49fc67edac49d5c2b98cf44ccbbe08319c26
ramonavic <ramon.hofmeister@gmail.com>
2016-08-22 10:30:28 +0200

    add style to nav and login

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import {Link} from 'react-router'
-
+import styles from '../assets/style/navigation.css.js'
+import Logout from './logout'
 
 class Navigation extends React.Component {
 
 
 render() {
   return (
-    <div>
-      <Link activeStyle={{color: '#fff'}} to='/'>Home</Link> &nbsp;
-      <Link activeStyle={{color: '#9CCC65'}} to='/Login'>Login</Link> &nbsp;
+      <div>
+        <ul style={styles.navList}>
+          <Link style={styles.navListItem} activeStyle={styles.navActive} to='/'>Home</Link> &nbsp;
+          <Link style={styles.navListItem} activeStyle={styles.navActive} to='/add'>Add Tutorial</Link> &nbsp;
+          <Link style={styles.navListItem} activeStyle={styles.navActive} to='/Login'>Login</Link> &nbsp;
+          <Logout />
+        </ul>
+      </div>
 
-    </div>
   )
 }
 
